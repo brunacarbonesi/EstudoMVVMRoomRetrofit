705595f414b6e39b00096a4fb4fa0f08fcc6d5ac
brunacarbonesi <brunacarbonesi@gmail.com>
2021-10-23 19:36:19 -0300

    Criação de classe com as constantes úteis

---
+++ app/src/main/java/com/brunacarbonesi/apps/estudomvvmroomretrofit/utils/AppConstants.kt
@@ -0,0 +1,10 @@
+package com.brunacarbonesi.apps.estudomvvmroomretrofit.utils
+
+public class AppConstants {
+
+    companion object {
+        const val URL: String = "https://api.themoviedb.org/3/"
+        const val API_KEY: String = "28462c086aa50bfdb7f5d824170f5df7"
+    }
+
+}
