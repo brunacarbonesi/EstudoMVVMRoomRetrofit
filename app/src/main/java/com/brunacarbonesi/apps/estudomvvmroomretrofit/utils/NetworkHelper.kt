f013b3f880f799af3066be9c028c60dbc39ede0b
brunacarbonesi <brunacarbonesi@gmail.com>
2021-10-23 19:53:33 -0300

    Checagem de conexão com internet/wifi

---
+++ app/src/main/java/com/brunacarbonesi/apps/estudomvvmroomretrofit/utils/NetworkHelper.kt
@@ -0,0 +1,32 @@
+package com.brunacarbonesi.apps.estudomvvmroomretrofit.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object NetworkHelper {
+    fun isNetworkConnected(context: Context): Boolean {
+
+        var result = false
+        (context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager).apply {
+            result = checkNetworkConnection(this, this.activeNetwork)
+        }
+        return result
+    }
+
+    private fun checkNetworkConnection(
+        connectivityManager: ConnectivityManager,
+        network: Network?
+    ): Boolean {
+        connectivityManager.getNetworkCapabilities(network)?.also {
+            if (it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                return true
+            } else if (it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                return true
+            }
+        }
+        return false
+    }
+}
