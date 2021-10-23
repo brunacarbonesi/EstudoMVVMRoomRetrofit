36592b674ce19c40def759e1e78a04eefdc70f17
brunacarbonesi <brunacarbonesi@gmail.com>
2021-10-23 19:54:41 -0300

    Classes de apoio para o ViewModel

---
+++ app/src/main/java/com/brunacarbonesi/apps/estudomvvmroomretrofit/utils/ViewData.kt
@@ -0,0 +1,6 @@
+package com.brunacarbonesi.apps.estudomvvmroomretrofit.utils
+
+data class ViewData<D> (
+    val data: D?  = null,
+    val viewStatus: ViewStatus
+)
---
+++ app/src/main/java/com/brunacarbonesi/apps/estudomvvmroomretrofit/utils/ViewStatus.kt
@@ -0,0 +1,8 @@
+package com.brunacarbonesi.apps.estudomvvmroomretrofit.utils
+
+enum class ViewStatus {
+    SUCCESS,
+    LOADING,
+    NO_VALUE,
+    ERROR
+}
